c83346f70baef9f3eaa75b93ab91046b049eba2a
OSS-GeoAIDev <pradeepkumawat@gmail.com>
2025-09-27 02:02:27 +0300

    addition of evaluation files

--- prod_assistant/retriever/retrieval.py
+++ prod_assistant/retriever/retrieval.py
@@ -1,83 +1,122 @@
 import os
-from langchain_core.documents import Document
-from langchain_astradb  import AstraDBVectorStore
-from typing import List
+from langchain_astradb import AstraDBVectorStore
 from utils.config_loader import load_config
 from utils.model_loader import ModelLoader
 from dotenv import load_dotenv
-import sys
-from pathlib import Path
-
+from langchain.retrievers.document_compressors import LLMChainFilter
+from langchain.retrievers import ContextualCompressionRetriever
+from evaluation.ragas_eval import evaluate_context_precision, evaluate_response_relevancy
 # Add the project root to the Python path for direct script execution
-project_root = Path(__file__).parent.parent.parent
-sys.path.append(str(project_root))
+# project_root = Path(__file__).resolve().parents[2]
+# sys.path.insert(0, str(project_root))
 
-class Retrieval:
-    """
-    Class to handle data retrieval from AstraDB vector store.
-    """
+class Retriever:
     def __init__(self):
+        """_summary_
         """
-        Initialize environment variables, embedding model, and AstraDB vector store.
-        """
-        self.model_loader = ModelLoader()
+        self.model_loader=ModelLoader()
+        self.config=load_config()
         self._load_env_variables()
-        self.config = load_config()
         self.vstore = None
-        self.retriever = None
+        self.retriever_instance = None
     
     def _load_env_variables(self):
+        """_summary_
         """
-        Load and validate required environment variables.
-        """ 
         load_dotenv()
-        
+         
         required_vars = ["GOOGLE_API_KEY", "ASTRA_DB_API_ENDPOINT", "ASTRA_DB_APPLICATION_TOKEN", "ASTRA_DB_KEYSPACE"]
         
         missing_vars = [var for var in required_vars if os.getenv(var) is None]
         
         if missing_vars:
             raise EnvironmentError(f"Missing environment variables: {missing_vars}")
-        
+
         self.google_api_key = os.getenv("GOOGLE_API_KEY")
         self.db_api_endpoint = os.getenv("ASTRA_DB_API_ENDPOINT")
         self.db_application_token = os.getenv("ASTRA_DB_APPLICATION_TOKEN")
         self.db_keyspace = os.getenv("ASTRA_DB_KEYSPACE")
     
     def load_retriever(self):
-        """
-        Load AstraDB vector store and create a retriever.
+        """_summary_
         """
         if not self.vstore:
             collection_name = self.config["astra_db"]["collection_name"]
             
-            print("Loading AstraDB vector store...")
-            self.vstore = AstraDBVectorStore(
-                embedding=self.model_loader.load_embeddings(),
+            self.vstore =AstraDBVectorStore(
+                embedding= self.model_loader.load_embeddings(),
                 collection_name=collection_name,
                 api_endpoint=self.db_api_endpoint,
                 token=self.db_application_token,
-                namespace=self.db_keyspace
-            )
-            print("AstraDB vector store loaded.")
-        if not self.retriever:
+                namespace=self.db_keyspace,
+                )
+        if not self.retriever_instance:
             top_k = self.config["retriever"]["top_k"] if "retriever" in self.config else 3
-            retriever = self.vstore.as_retriever(search_kwargs={"k": top_k})
-            print(f"Retriever loaded successfully")
-            return retriever
-        
+            
+            mmr_retriever=self.vstore.as_retriever(
+                search_type="mmr",
+                search_kwargs={"k": top_k,
+                                "fetch_k": 20,
+                                "lambda_mult": 0.7,
+                                "score_threshold": 0.6
+                               })
+            print("Retriever loaded successfully.")
+            
+            llm = self.model_loader.load_llm()
+            
+            compressor=LLMChainFilter.from_llm(llm)
+            
+            self.retriever_instance = ContextualCompressionRetriever(
+                base_compressor=compressor, 
+                base_retriever=mmr_retriever
+            )
+            
+        return self.retriever_instance
+            
     def call_retriever(self,query):
+        """_summary_
         """
-        Retrieve documents based on the query.
-        """
-        retriever = self.load_retriever()
-        output = retriever.invoke(query)
+        retriever=self.load_retriever()
+        output=retriever.invoke(query)
         return output
-
-if __name__ == "__main__":
-    retriever_obj = Retrieval()
-    user_query = "What are the top features of the latest smartphone models?"
-    result = retriever_obj.call_retriever(user_query)
     
-    for idx, doc in enumerate(result, 1):
-        print(f"Result {idx}: {doc.page_content}\nMetadata: {doc.metadata}\n")
+if __name__=='__main__':
+    user_query = "Can you suggest good budget iPhone under 1,00,00 INR?"
+    
+    retriever_obj = Retriever()
+    
+    retrieved_docs = retriever_obj.call_retriever(user_query)
+    
+    def _format_docs(docs) -> str:
+        if not docs:
+            return "No relevant documents found."
+        formatted_chunks = []
+        for d in docs:
+            meta = d.metadata or {}
+            formatted = (
+                f"Title: {meta.get('product_title', 'N/A')}\n"
+                f"Price: {meta.get('price', 'N/A')}\n"
+                f"Rating: {meta.get('rating', 'N/A')}\n"
+                f"Reviews:\n{d.page_content.strip()}"
+            )
+            formatted_chunks.append(formatted)
+        return "\n\n---\n\n".join(formatted_chunks)
+    
+    retrieved_contexts = [_format_docs(doc) for doc in retrieved_docs]
+    
+    #this is not an actual output this have been written to test the pipeline
+    response="iphone 16 plus, iphone 16, iphone 15 are best phones under 1,00,000 INR."
+    
+    context_score = evaluate_context_precision(user_query,response,retrieved_contexts)
+    relevancy_score = evaluate_response_relevancy(user_query,response,retrieved_contexts)
+    
+    print("\n--- Evaluation Metrics ---")
+    print("Context Precision Score:", context_score)
+    print("Response Relevancy Score:", relevancy_score)
+    
+
+    
+    
+    
+    # for idx, doc in enumerate(results, 1):
+    #     print(f"Result {idx}: {doc.page_content}\nMetadata: {doc.metadata}\n")
